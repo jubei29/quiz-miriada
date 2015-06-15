14879843f2f6925bc835d015e70aadb9721f4871
jubei29 <jubei29@gmail.com>
2015-06-15 13:05:01 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -116,3 +116,33 @@ exports.create = function(req, res) {
 		}
 	});
 }
+
+// GET /quizes/edit
+exports.edit = function(req, res) {
+	res.render('quizes/edit', {
+		quiz: req.quiz
+	});
+}
+
+// PUT /quizes
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(function (err) {
+		if (err) {
+			res.render('quizes/edit', {
+				quiz   : req.quiz,
+				errors : err.errors
+			});
+		} else {
+			req.quiz.save({
+				fields : ['pregunta', 'respuesta']
+			}).then(function () {
+				res.redirect('/quizes');
+			});
+		}
+	});
+}
